8b19967fe6f225cc7f26c61f1dd68dfd3168a86f
Alan Kay <alanajkay@gmail.com>
2020-03-02 20:20:29 -0600

    Fixed inconsistent usage of apt-get vs apt

--- provision/install-netcore.sh
+++ provision/install-netcore.sh
@@ -5,7 +5,7 @@ set -ex
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y dotnet-sdk-3.1
+sudo apt update
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install -y dotnet-sdk-3.1
