011406724ecbf947f3eab42058661442c3048325
Alan Kay <alanajkay@gmail.com>
2020-03-03 00:04:16 -0600

    fixed terminus

--- provision/install-terminus.sh
+++ provision/install-terminus.sh
@@ -2,4 +2,4 @@
 
 # Install terminus
 wget https://github.com/Eugeny/terminus/releases/download/v1.0.104/terminus-1.0.104-linux.deb
-sudo dpkg -i terminus-1.0.104-linux.deb
+sudo apt install -y -f ~/terminus-1.0.104-linux.deb
