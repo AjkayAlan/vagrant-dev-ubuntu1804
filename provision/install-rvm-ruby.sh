ec5d255eb48c3e1ce4fec8be512b48c6b7130f11
Alan Kay <alanajkay@gmail.com>
2020-03-02 23:34:25 -0600

    Fixed ruby.  Bumped back perms to use privelaged false

--- provision/install-rvm-ruby.sh
+++ provision/install-rvm-ruby.sh
@@ -1,16 +1,24 @@
 #!/bin/zsh
 
 # Install dependencies
-sudo apt install -y software-properties-common
+sudo apt install -y \
+    software-properties-common \
+    gnupg2 \
+    awk \
+    sed \
+    grep \
+    which \
+    ls \
+    cp \
+    tar \
+    curl \
+    gunzip \
+    bunzip2
 
 # Install RVM
-sudo apt-add-repository -y ppa:rael-gc/rvm
-sudo apt update
-sudo apt install -y rvm
-
-# Make RVM accessible
-echo 'source "/etc/profile.d/rvm.sh"' >> ~/.zshrc
+gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
 source ~/.zshrc
 
-# Install ruby
-rvm install ruby
+# Install Ruby
+rvm install ruby --latest
