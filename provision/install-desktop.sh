801ded9622d6709ae6c3fab7fc4f3e11f898811e
Alan Kay <alanajkay@gmail.com>
2020-03-03 18:06:07 -0600

    Switched from taskel to apt

--- provision/install-desktop.sh
+++ provision/install-desktop.sh
@@ -3,8 +3,5 @@
 # Set default ubuntu user
 echo "ubuntu:ubuntu" | sudo chpasswd
 
-# Install tasksel
-sudo apt install -y tasksel
-
-# Install gnome desktop
-sudo tasksel install ubuntu-desktop
+# Install ubuntu's desktop
+sudo apt install -y ubuntu-desktop
