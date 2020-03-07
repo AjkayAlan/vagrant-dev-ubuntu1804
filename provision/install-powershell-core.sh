fe7c6f1eb300264e494741fdc4c25cdedb135897
Alan Kay <alanajkay@gmail.com>
2020-03-06 20:18:49 -0600

    added some fixes for ruby, made apt-get consistent

--- provision/install-powershell-core.sh
+++ provision/install-powershell-core.sh
@@ -7,10 +7,10 @@ wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 
 # Update the list of products
-sudo apt-get update
+sudo apt update
 
 # Enable the "universe" repositories
 sudo add-apt-repository universe
 
 # Install PowerShell
-sudo apt-get install -y powershell
+sudo apt install -y powershell
